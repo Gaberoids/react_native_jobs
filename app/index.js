628b8da50e2913a2cb2adca921693c71314ff212
Gaberoids <figueiropas@hotmail.com>
2023-04-06 23:01:42 -0600

    Added a menu icon and a profile icon (not working)

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native';
+// Home page
+import {useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+// from folders folders on work space
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import { setStatusBarBackgroundColor } from 'expo-status-bar';
+
 
 const Home = () => {
+    // creating a layout start with a router
+    const router = useRouter();
     return (
-        <View>
+        <SafeAreaView styles={{flex: 1, setStatusBarBackgroundColor: COLORS.lightWhite }}>
             <Text>Home</Text>
-        </View>
+            <Stack.Screen options={{ 
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrL={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrL={icons.profile} dimension="100%" />
+                ),
+                 }} />
+        </SafeAreaView>
+        // headershadowview hides the header if there is a header "INDEX" on the page.
     )
 }
 
